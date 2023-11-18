c3c1201be24bcdfe5375ffdcd2d189a074a44908
Lucas Celestino <lucascap899@gmail.com>
2023-11-17 19:40:09 -0800

    criando funcao para consumir a api e alterando estrutura do componente produto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import Produto from './Produto';
 
 const App = () => {
+
+  const [dados, setDados] = React.useState(null);
+
+  async function handleClick(event)
+  {
+    const productUrl = event.target.innerText.toLowerCase();
+
+    const response = await fetch('https://ranekapi.origamid.dev/json/api/produto/'+productUrl);
+
+    const productJson = await response.json();
+
+    setDados(productJson);
+  }
+
   return (
     <div style={{paddingTop:'70px', paddingLeft:'300px'}}>
-      <button style={{marginRight:'20px', backgroundColor:'greenyellow'}}>Notebook</button>
-      <button style={{marginRight:'20px', backgroundColor:'greenyellow'}}>Smartphone</button>
-      <button style={{marginRight:'20px', backgroundColor:'greenyellow'}}>Tablet</button>
+      <Produto dados={dados} />
+      <button onClick={handleClick} style={{marginRight:'20px', backgroundColor:'greenyellow'}}>Notebook</button>
+      <button onClick={handleClick} style={{marginRight:'20px', backgroundColor:'greenyellow'}}>Smartphone</button>
+      <button onClick={handleClick} style={{marginRight:'20px', backgroundColor:'greenyellow'}}>Tablet</button>
     </div>
   )
 }
--- src/Produto.js
+++ src/Produto.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
-function Produto({dados}) {
-
-  return (
-    <div>
-      <h1>{dados.nome}</h1>
-      <p>{dados.descricao}</p>
-      <p>{dados.preco}</p>
-      <img src={dados.fotos[0].src}></img>
-    </div>
-  )
+const Produto = ({dados}) => {
+    return(
+        <div>
+        <h1>{dados.nome}</h1>
+        <p>{dados.descricao}</p>
+        <p>R$ {dados.preco}</p>
+        <img src={dados.fotos[0].src}></img>
+        </div>
+    );
 }
 
 export default Produto;
