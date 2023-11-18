707dc4d2c292fc41c53bb3e7ab417b2471b4e2c1
Lucas Celestino <lucascap899@gmail.com>
2023-11-17 19:48:13 -0800

    criando mensagem de loading enquanto a requisicao da api é realizada

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import Produto from './Produto';
 const App = () => {
 
   const [dados, setDados] = React.useState(null);
+  const [loading, setLoading] = React.useState(null);
 
   async function handleClick(event)
   {
+
+    setLoading(true);
+
     const productUrl = event.target.innerText.toLowerCase();
 
     const response = await fetch('https://ranekapi.origamid.dev/json/api/produto/'+productUrl);
@@ -14,11 +18,14 @@ const App = () => {
     const productJson = await response.json();
 
     setDados(productJson);
+
+    setLoading(false);
   }
 
   return (
     <div style={{paddingTop:'70px', paddingLeft:'300px'}}>
-      <Produto dados={dados} />
+      {loading && 'Carregando...'}
+      {!loading && dados && <Produto dados={dados} />}
       <button onClick={handleClick} style={{marginRight:'20px', backgroundColor:'greenyellow'}}>Notebook</button>
       <button onClick={handleClick} style={{marginRight:'20px', backgroundColor:'greenyellow'}}>Smartphone</button>
       <button onClick={handleClick} style={{marginRight:'20px', backgroundColor:'greenyellow'}}>Tablet</button>
